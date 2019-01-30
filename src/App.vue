37526fb226de466fb5e07795a350f4f90ea994ff
hirodashi <zacharydurland@gmail.com>
2019-01-29 21:45:01 -0600

    add working video detail when video is selected from list

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,26 @@
   <div id="App" class="container">
     <SearchBar @termChange="onTermChange"></SearchBar>
     <div class="row">
-        <div class="col-md-7">
-            <VideoDetail :video="selectedVideo" />
-        </div>
-        <div class="col-md-4 col-md-offset-1">
-            <VideoList @videoSelect="onVideoSelect" :videos="videos"></VideoList>
-        </div>
+      <div class="col-md-7">
+        <VideoDetail
+          v-if="selectedVideo"
+          :video="selectedVideo.snippet.title"
+          :videoId="selectedVideo.id.videoId"
+        />
+      </div>
+      <div class="col-md-4 col-md-offset-1">
+        <VideoList @videoSelect="onVideoSelect" :videos="videos"></VideoList>
+      </div>
     </div>
   </div>
 </template>
-
-
 
 <script>
 import Axios from "axios";
 import SearchBar from "./components/SearchBar";
 import VideoList from "./components/VideoList";
 import VideoDetail from "./components/VideoDetail";
-const API_KEY = "";
+// import API_KEY from "../KEY_YOUTUBE";
 
 export default {
   name: "App",
@@ -41,18 +43,18 @@ export default {
     onTermChange(searchTerm) {
       Axios.get("https://www.googleapis.com/youtube/v3/search", {
         params: {
-          key: API_KEY,
+          key: "AIzaSyBFjzdSU-qJfO7COWM3FCITOwJvgF8Y88I",
           type: "video",
           part: "snippet",
           q: searchTerm
         }
       }).then(response => {
         this.videos = response.data.items;
+        console.log(this.videos[0]);
       });
     }
   }
 };
 </script>
 
-<style>
-</style>
+<style></style>
--- src/components/VideoDetail.vue
+++ src/components/VideoDetail.vue
@@ -1,16 +1,33 @@
 <template>
-    <div v-if="video">
-        {{video.snippet.title}}
+  <div v-if="video">
+    {{ title }}
+    <div class="embed-responsive embed-responsive-16by9">
+      <iframe
+        id="ytplayer"
+        type="text/html"
+        width="720"
+        height="405"
+        :src="this.videoURL"
+        frameborder="0"
+        allowfullscreen
+      >
+      </iframe>
     </div>
+  </div>
 </template>
-
 
 <script>
 export default {
-    name: 'VideoDetail',
-    props: ['video']
-}
+  name: "VideoDetail",
+  props: ["title", "videoId", "video"],
+  computed: {
+    videoURL() {
+      const url = "https://www.youtube.com/embed/" + this.videoId;
+      console.log(url);
+      return url;
+    }
+  }
+};
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
